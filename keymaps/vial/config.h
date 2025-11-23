f08cf5f5e56dcd5cb3bf5df56f31770afb2ecbcb
bharasyah <bharasyahhendra@gmail.com>
2025-11-24 06:31:38 +0700

    update vial
    support vial dan smoothness improved trackpoint

---
+++ keymaps/vial/config.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+
+#pragma once
+
+#define VIAL_KEYBOARD_UID {0xE0, 0x1B, 0x89, 0x85, 0x41, 0x8D, 0xB9, 0x36}
+#define VIAL_UNLOCK_COMBO_ROWS { 0, 2 }
+#define VIAL_UNLOCK_COMBO_COLS { 0, 12 }
